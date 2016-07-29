1eaf4210ca10410abecc74db7bd22df34da2dff8
Shannon MacMillan <shannon@webdevstudios.com>
2016-07-29 08:47:31 -0700

    Initial commit.

---
+++ includes/class-bsc-brand-colors-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://bluestormcreative.com
+ * @since      1.0.0
+ *
+ * @package    Bsc_Brand_Colors
+ * @subpackage Bsc_Brand_Colors/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Bsc_Brand_Colors
+ * @subpackage Bsc_Brand_Colors/includes
+ * @author     Blue Storm Creative <shannon@bluestormcreative.com>
+ */
+class Bsc_Brand_Colors_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
