1d09a4323a462afe6ec35e21c83b9d065198cc18
Shannon MacMillan <shannon@webdevstudios.com>
2016-11-04 12:34:00 -0700

    Add menu buttons and function to wrap text in color span

--- admin/js/bsc-brand-colors-tinymce-buttons.js
+++ admin/js/bsc-brand-colors-tinymce-buttons.js
@@ -1,5 +1,11 @@
-(function() {
-    tinymce.PluginManager.add('bsc_bc_tinymce_button', function( editor, url ) {
+(function($) {
+
+    //php_vars.brand_colors
+    //
+    //console.log(php_vars.brand_colors['primary-color']);
+
+    tinyMCE.PluginManager.add('bsc_bc_tinymce_button', function( editor, url ) {
+
         //this command will be executed when the button in the toolbar is clicked
         editor.addCommand('mceWRAP', function() {
 
@@ -8,6 +14,7 @@
             tinyMCE.activeEditor.selection.setContent('<span class="brand-color">' + selection + '</span>');
 
         });
+
         editor.addButton( 'bsc_bc_tinymce_button', {
             text: 'Brand Colors',
             type: 'menubutton',
@@ -15,10 +22,33 @@
             menu: [
                 {
                     text: 'Primary Color',
-                    value: '#ff0000;',
+                    value: php_vars.brand_colors['primary-color'],
+                    onclick: function() {
+                        wrapSelection( this.value() );
+                    }
+                },
+                {
+                    text: 'Second Color',
+                    value: php_vars.brand_colors['second-color'],
+                    onclick: function() {
+                        wrapSelection( this.value() );
+                    }
+                },
+                {
+                    text: 'Third Color',
+                    value: php_vars.brand_colors['third-color'],
+                    onclick: function() {
+                        wrapSelection( this.value() );
+                    }
                 }
-            ],
-            cmd : 'mceWRAP'
+            ]
         });
+
+        function wrapSelection( color ) {
+
+            selection = tinyMCE.activeEditor.selection.getContent();
+
+            tinyMCE.activeEditor.selection.setContent('<span class="brand-color" style="color: ' + color + '">' + selection + '</span>');
+        }
     });
-})();
+})(jQuery);
