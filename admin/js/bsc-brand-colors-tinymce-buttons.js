6ba66d80aef1e72e0d4b5ee9d2892e59ceedf303
Shannon MacMillan <shannon@webdevstudios.com>
2016-11-04 15:19:03 -0700

    Refactor tinymce init code

--- admin/js/bsc-brand-colors-tinymce-buttons.js
+++ admin/js/bsc-brand-colors-tinymce-buttons.js
@@ -1,7 +1,15 @@
 (function() {
 
-    tinymce.PluginManager.add('bsc_bc_tinymce_button', function( editor, url ) {
-
+    tinymce.create('tinymce.plugins.BSC_BC', {
+    /**
+     * Initializes the plugin, this will be executed after the plugin has been created.
+     * This call is done before the editor instance has finished it's initialization so use the onInit event
+     * of the editor instance to intercept that event.
+     *
+     * @param {tinymce.Editor} ed Editor instance that the plugin is initialized in.
+     * @param {string} url Absolute URL to where the plugin is located.
+     */
+    init : function(editor, url) {
 
         editor.addButton( 'bsc_bc_tinymce_button', {
             text: 'Brand Colors',
@@ -60,6 +68,26 @@
             tinyMCE.activeEditor.selection.setContent('<span class="brand-color" style="color: ' + color + '">' + selection + '</span>');
         }
 
-    });
+    },
+
+    /**
+     * Returns information about the plugin as a name/value array.
+     * The current keys are longname, author, authorurl, infourl and version.
+     *
+     * @return {Object} Name/value array containing information about the plugin.
+     */
+    getInfo : function() {
+        return {
+            longname : 'BSC Brand Color Button',
+            author : 'BlueStormCreative',
+            authorurl : 'https://bluestormcreative.com',
+            version : "0.1"
+        };
+    }
+});
+
+// Register plugin
+tinymce.PluginManager.add( 'bsc_bc_tinymce_button', tinymce.plugins.BSC_BC );
+
 
 })();
