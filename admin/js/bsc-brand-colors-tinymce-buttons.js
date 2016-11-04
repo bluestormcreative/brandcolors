41687d816da39dd65915acb54328787ad3d346bd
Shannon MacMillan <shannon@webdevstudios.com>
2016-11-04 12:04:11 -0700

    Add function to wrap text in span

--- admin/js/bsc-brand-colors-tinymce-buttons.js
+++ admin/js/bsc-brand-colors-tinymce-buttons.js
@@ -1,18 +1,24 @@
 (function() {
     tinymce.PluginManager.add('bsc_bc_tinymce_button', function( editor, url ) {
+        //this command will be executed when the button in the toolbar is clicked
+        editor.addCommand('mceWRAP', function() {
+
+            selection = tinyMCE.activeEditor.selection.getContent();
+
+            tinyMCE.activeEditor.selection.setContent('<span class="brand-color">' + selection + '</span>');
+
+        });
         editor.addButton( 'bsc_bc_tinymce_button', {
             text: 'Brand Colors',
             type: 'menubutton',
             icon: 'icon dashicons-art',
             menu: [
-                  {
-                      text: 'Menu item I',
-                      value: 'Text from menu item I',
-                      onclick: function() {
-                          editor.insertContent(this.value());
-                      }
-                  }
-             ]
+                {
+                    text: 'Primary Color',
+                    value: '#ff0000;',
+                }
+            ],
+            cmd : 'mceWRAP'
         });
     });
 })();
