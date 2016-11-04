0b13c36691fb98dc5e82c1b5954a6728ed28d0de
Shannon MacMillan <shannon@webdevstudios.com>
2016-11-04 11:55:38 -0700

    Add type menubutton to our button

--- admin/js/bsc-brand-colors-tinymce-buttons.js
+++ admin/js/bsc-brand-colors-tinymce-buttons.js
@@ -1,11 +1,18 @@
 (function() {
     tinymce.PluginManager.add('bsc_bc_tinymce_button', function( editor, url ) {
         editor.addButton( 'bsc_bc_tinymce_button', {
-            text: 'My test button',
-            icon: false,
-            onclick: function() {
-                editor.insertContent('Hello World!');
-            }
+            text: 'Brand Colors',
+            type: 'menubutton',
+            icon: 'icon dashicons-art',
+            menu: [
+                  {
+                      text: 'Menu item I',
+                      value: 'Text from menu item I',
+                      onclick: function() {
+                          editor.insertContent(this.value());
+                      }
+                  }
+             ]
         });
     });
 })();
