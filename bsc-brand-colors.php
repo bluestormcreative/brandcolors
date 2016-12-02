76cf778ed6ee3d6f5d87d1a3b3b82a49ab39838c
Shannon MacMillan <shannon@webdevstudios.com>
2016-12-02 12:42:28 -0700

    Add colorpicker/input to table markup

--- bsc-brand-colors.php
+++ bsc-brand-colors.php
@@ -136,10 +136,18 @@ function bsc_bc_setup_admin_page() {
 
 					<table id='bsc-brand-colors-editor' aria-hidden='###' class='form-table'>
 					  <tbody>
-						<tr><td id='bsc-brand-colors-toolbar' role='toolbar'>
-						  <button id='bsc-brand-colors-add-button' type='submit' tabindex='8' name='bsc-action' value='add' class='button' aria-controls='bsc-brand-colors' accesskey='###'>Add Color</button>
-						</td></tr>
-						<tr><td id='kt_colors' data-empty='" . esc_attr__('Palette is empty', 'bsc-brand-colors') . "'>picker here</td></tr>
+						<tr>
+							<td id='bsc-brand-colors-toolbar' role='toolbar'>
+						  	<button id='bsc-brand-colors-add-button' type='submit' tabindex='8' name='bsc-action' value='add' class='button'>Add Brand Color</button>
+							</td>
+						</tr>
+						<tr>
+							<td id='bsc-brand-colors-color-4'>
+								<input type="text" id="bsc_brand_colors-color-4" name="bsc_brand_colors[color-4]" class="bsc-color-picker color-field" value="<?php if ( isset( $color_array['color-4'] ) ) { echo esc_html( $color_array['color-4'], 'bsc-brand-colors' ); } ?>" />
+
+								<input type="text" id="bsc_brand_colors-third-color-4-label" size="25" name="bsc_brand_colors[color-4-label]" class="bsc-color-label" value="<?php if ( isset( $color_array['color-4-label'] ) ) { echo esc_html( $color_array['color-4-label'], 'bsc-brand-colors'); } ?>" placeholder="Name this color" />
+							</td>
+						</tr>
 					  </tbody>
 					</table>
 	            </fieldset>
