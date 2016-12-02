2dc726a9516d72c65c76ab17a045105cecb2f9d0
Shannon MacMillan <shannon@webdevstudios.com>
2016-12-02 12:48:07 -0700

    Add some js to duplicate table rows on button click

--- admin/js/bsc-brand-colors-admin.js
+++ admin/js/bsc-brand-colors-admin.js
@@ -1,10 +1,27 @@
 (function( $ ) {
 	'use strict';
 
-    // Add Color Picker to all inputs that have 'color-field' class.
-    $(function() {
-        $('.color-field').wpColorPicker();
-    });
+	// Add Color Picker to all inputs that have 'color-field' class.
+	$(function() {
+		$('.color-field').wpColorPicker();
+	});
 
+	// Append another table row when Add Brand Color button clicked
+	$('.bsc-add-button').on('click', function(){
+		$('#bsc-brand-colors-editor').each( function() {
+			var tds = '<tr>';
+			$.each( $( 'tr:last td', this ), function() {
+				tds += '<td>' + $( this ).html() + '</td>';
+			});
+			
+			tds += '</tr>';
+
+			if ( $('tbody', this ).length > 0 ) {
+				$('tbody', this ).append( tds );
+			} else {
+				$(this).append( tds );
+			}
+		});
+	});
 
 })( jQuery );
